91433f92a3cd592d2e55effe188861636a40173e
Jake Peterson <jpeterson0514@gmail.com>
2020-09-19 02:54:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,73 @@
-# Write your code here!
+def game_hash 
+  {
+    :home => {
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "White"],
+      players: [{
+        player_name: "",
+        number: "",
+        shoe: "",
+        points: "",
+        assists: "",
+        steals: "",
+        blocks: "",
+        slam_dunks: ""
+      },
+      {
+        player_name: "",
+        number: "",
+        shoe: "",
+        points: "",
+        assists: "",
+        steals: "",
+        blocks: "",
+        slam_dunks: ""
+      },
+      {
+        player_name: "",
+        number: "",
+        shoe: "",
+        points: "",
+        assists: "",
+        steals: "",
+        blocks: "",
+        slam_dunks: ""
+      },
+      {
+        player_name: "",
+        number: "",
+        shoe: "",
+        points: "",
+        assists: "",
+        steals: "",
+        blocks: "",
+        slam_dunks: ""
+      },
+      {
+        player_name: "",
+        number: "",
+        shoe: "",
+        points: "",
+        assists: "",
+        steals: "",
+        blocks: "",
+        slam_dunks: ""
+      }
+      ]
+    },
+    :away =>{
+      team_name: "",
+      colors: [""],
+      players: [{
+        player_name: "",
+        number: "",
+        shoe: "",
+        points: "",
+        assists: "",
+        steals: "",
+        blocks: "",
+        slam_dunks: ""
+      }]
+    }
+  }
+end
