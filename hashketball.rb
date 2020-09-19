27e89294c8f6a045de53b3320451deaa39ab5ce3
Jake Peterson <jpeterson0514@gmail.com>
2020-09-19 03:20:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,36 +5,36 @@ def game_hash
       colors: ["Black", "White"],
       players: [{
         player_name: "Alan Anderson",
-        number: "0",
-        shoe: "16",
-        points: "22",
-        rebounds: "12",
-        assists: "12",
-        steals: "3",
-        blocks: "1",
-        slam_dunks: "1"
+        number: 0,
+        shoe: 16,
+        points: 22,
+        rebounds: 12,
+        assists: 12,
+        steals: 3,
+        blocks: 1,
+        slam_dunks: 1
       },
       {
         player_name: "Reggie Evans",
-        number: "30",
-        shoe: "14",
-        points: "12",
-        rebounds: "12",
-        assists: "12",
-        steals: "12",
-        blocks: "12",
-        slam_dunks: "7"
+        number: 30,
+        shoe: 14,
+        points: 12,
+        rebounds: 12,
+        assists: 12,
+        steals: 12,
+        blocks: 12,
+        slam_dunks: 7
       },
       {
         player_name: "Brook Lopez",
-        number: "11",
-        shoe: "17",
-        points: "17",
-        rebounds: "19",
-        assists: "10",
-        steals: "3",
-        blocks: "1",
-        slam_dunks: "15"
+        number: 11,
+        shoe: 17,
+        points: 17,
+        rebounds: 19,
+        assists: 10,
+        steals: 3,
+        blocks: 1,
+        slam_dunks: 15
       },
       {
         player_name: "Mason Plumlee",
@@ -49,14 +49,14 @@ def game_hash
       },
       {
         player_name: "Jason Terry",
-        number: "31",
-        shoe: "15",
-        points: "19",
-        rebounds: "2",
-        assists: "2",
-        steals: "4",
-        blocks: "11",
-        slam_dunks: "1"
+        number: 31,
+        shoe: 15,
+        points: 19,
+        rebounds: 2,
+        assists: 2,
+        steals: 4,
+        blocks: 11,
+        slam_dunks: 1
       }
       ]
     },
@@ -65,9 +65,9 @@ def game_hash
       colors: ["Torquoise", "Purple"],
       players: [{
         player_name: "Jeff Adrien",
-        number: "4",
-        shoe: "18",
-        points: "10",
+        number: 4,
+        shoe: 18,
+        points: 10,
         rebounds: "1",
         assists: "1",
         steals: "2",
